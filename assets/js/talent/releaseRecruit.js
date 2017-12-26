52884d5833e54515b23d722a91be42e0dcccc483
javency <635142482@qq.com>
2017-12-26 14:38:33 +0800

    完善发布招聘_提交后台

--- assets/js/talent/releaseRecruit.js
+++ assets/js/talent/releaseRecruit.js
@@ -30,6 +30,50 @@ $(function() {
         }
     });
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //过滤函数（如果为空，自动补零）
+    function autoAddZero(str) {
+        var strFilter = null;
+        return strFilter = (str == '') ? 0 : str;
+    }
+
+    //输入框传后台带标签
+    function autoChangeStyle(obj) {
+        var val = $(obj).val();
+        var reg = new RegExp("\n", "g")
+        val = val.replace(reg, "</p><p>");
+        return val = "<p>" + val + "</p>";
+        console.log(val);
+    }
+
+    $("#next").on('click', function() {
+        var insertData = {
+            'Name': autoAddZero($("#enterpriseName").val()),
+            'Intro': autoAddZero(autoChangeStyle("#corporateProfile")),
+            'Brief_intro': autoAddZero($("#oneSentence").val()),
+            'Province': autoAddZero($("#selProvince").val()),
+            'City': autoAddZero($("#selCity").val()),
+            'Site': autoAddZero($("#workSpot").val()),
+            'Link_name': autoAddZero($("#name").val()),
+            'Link_phone': autoAddZero($("#tel").val()),
+            'Link_mail': autoAddZero($("#mail").val())
+        }
+        $.ajax({
+            type: 'post',
+            async: true,
+            traditional: true,
+            dataType: 'jsonp',
+            jsonp: 'Callback',
+            url: 'http://' + changeUrl.address + '/Recuitment_api?whereFrom=insert',
+            data: insertData,
+            success: function() {
+                console.log(1)
+            }
+        })
+    })
+
+
     //监听用户输入字数
     $("#corporateProfile").keyup(function() {
         var wordTotal = 100,
